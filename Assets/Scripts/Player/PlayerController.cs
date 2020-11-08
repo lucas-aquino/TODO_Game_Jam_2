0deb46e0fab1f880bc4730ab44ff2b7191240592
Lucas Aquino <luckasakino@protonmail.com>
2020-11-08 02:53:54 -0300

    Setup Movement

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -1,11 +1,12 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 
 
 public class PlayerController : MonoBehaviour
 {
 
     [Header("Movement")]
-    [SerializeField] float playerSpeed = 200;
+    [SerializeField] float playerSpeed = 20;
     [SerializeField] float playerJumpForce = 200;
 
     [Header("Ground Check")]
@@ -19,7 +20,7 @@ public class PlayerController : MonoBehaviour
     
     Rigidbody2D playerRb;
 
-    Vector2 movement;
+    //Vector2 movement;
 
     private void Awake()
     {
@@ -33,6 +34,12 @@ public class PlayerController : MonoBehaviour
 
     private void Move()
     {
+        playerRb.velocity = new Vector2(Input.GetAxis("Horizontal") * playerSpeed, playerRb.velocity.y);
+
+
+        if (isGrounded && (Input.GetKey(KeyCode.Space) || Input.GetKey(KeyCode.UpArrow)))
+            playerRb.velocity = new Vector2(playerRb.velocity.x, playerJumpForce);
+        
         
     }
 
